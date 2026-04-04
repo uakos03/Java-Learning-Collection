539d5c998e68e0912efdcb0c8158fe031dec064c
uakos03 <urbanakos12@gmail.com>
2026-04-04 17:38:43 +0200

    Variables added

--- Szoftver_vizsga/src/Szoftver.java
+++ Szoftver_vizsga/src/Szoftver.java
@@ -1,7 +1,12 @@
+import java.time.LocalDate;
+
 public class Szoftver {
+    private final String nev;
+    private int ar;
+    private String[] tamogatottOS;
+    private LocalDate date;
 
 
 
-    //ososztlay1
-
+    //ososztlayy
 }
