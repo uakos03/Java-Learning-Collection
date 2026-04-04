44f4c6f7a67fe715cf5acf5ee9c62f4c92855a50
uakos03 <urbanakos12@gmail.com>
2026-04-04 19:13:21 +0200

    Software class halfway

--- Szoftver_vizsga/src/Szoftver.java
+++ Szoftver_vizsga/src/Szoftver.java
@@ -1,15 +1,85 @@
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Szoftver {
     private final String nev;
     private int ar;
     private String[] tamogatottOS;
     private LocalDate date;
+
+
+    private Tipus tipus;
+
     public enum Tipus {
         IRODAI, JATEK, MEDIA
     }
 
 
+    public Szoftver(String nev, int ar, Tipus tipus, String[] tamogatottOS, LocalDate date) {
+        this.nev = nev;
+        this.ar = ar;
+        this.tipus = tipus;
+        this.tamogatottOS = tamogatottOS;
+        this.date = date;
+    }
 
-    //ososztlayy
+
+    public Szoftver(String nev, String[] tamogatottOS) {
+        this(
+                nev,
+                new Random().nextInt(10001) + 10000,
+                Tipus.IRODAI,
+                tamogatottOS,
+                LocalDate.now()
+        );
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public int getAr() {
+        return ar;
+    }
+
+    public void setAr(int ar) {
+        this.ar = ar;
+    }
+
+    public String[] getTamogatottOS() {
+        return tamogatottOS;
+    }
+
+    public void setTamogatottOS(String[] tamogatottOS) {
+        this.tamogatottOS = tamogatottOS;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Tipus getTipus() {
+        return tipus;
+    }
+
+    public void setTipus(Tipus tipus) {
+        this.tipus = tipus;
+    }
+
+    @Override
+    public String toString() {
+        return "Szoftver{" +
+                "nev='" + nev + '\'' +
+                ", ar=" + ar +
+                ", tamogatottOS=" + Arrays.toString(tamogatottOS) +
+                ", date=" + date +
+                ", tipus=" + tipus +
+                '}';
+    }
+    
 }
