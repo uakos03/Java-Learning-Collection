d992f774c146fd80d8e1da9ed468aa6d4c8cb641
uakos03 <urbanakos12@gmail.com>
2026-05-02 19:58:31 +0200

    _

--- Szoftver_vizsga/src/JatekSzoftver.java
+++ Szoftver_vizsga/src/JatekSzoftver.java
@@ -2,11 +2,12 @@ import java.time.LocalDate;
 
 public class JatekSzoftver extends Szoftver {
     private int korhatar;
+
+
     private String name;
-    private int id;
 
-    private double balance;
 
+    //++
     
 
 
